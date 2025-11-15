ca46659f1c3c0a1f5058453dfb898fb709daac23
Abinand P <abinand0911@gmail.com>
2025-11-15 07:57:11 +0530

    chore: removed unused column files and also resolved the type conflicts
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- frontend/src/features/functions/components/ScheduleExecutionLogs.tsx
+++ frontend/src/features/functions/components/ScheduleExecutionLogs.tsx
@@ -64,7 +64,7 @@ export function ScheduleExecutionLogs({
         ),
       },
       {
-        key: 'durationMs',
+        key: 'endTime',
         name: 'End Time',
         width: '200px',
         renderCell: ({ row }) => {
