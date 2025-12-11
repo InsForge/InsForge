501299110eb9019dcfee4e71f6d8fc9b0b0622f1
Lyu <fermioniclyu@gmail.com>
2025-12-10 16:23:45 -0800

    fix import paths

--- frontend/src/features/realtime/index.ts
+++ frontend/src/features/realtime/index.ts
@@ -1,6 +1,6 @@
-export { default as RealtimeChannelsPage } from './page/RealtimeChannelsPage';
-export { default as RealtimeMessagesPage } from './page/RealtimeMessagesPage';
-export { default as RealtimePermissionsPage } from './page/RealtimePermissionsPage';
+export { default as RealtimeChannelsPage } from './pages/RealtimeChannelsPage';
+export { default as RealtimeMessagesPage } from './pages/RealtimeMessagesPage';
+export { default as RealtimePermissionsPage } from './pages/RealtimePermissionsPage';
 export { useRealtime } from './hooks/useRealtime';
 export { realtimeService } from './services/realtime.service';
 export type {
