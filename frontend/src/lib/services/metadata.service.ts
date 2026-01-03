9159fd55dd5e7e8f8c0b8113d24d049d5fad421a
Leo-rq-yu <leoyuruiqing@gmail.com>
2026-01-02 17:04:08 -0800

    Connect with backend for connetion string

--- frontend/src/lib/services/metadata.service.ts
+++ frontend/src/lib/services/metadata.service.ts
@@ -1,6 +1,23 @@
 import { apiClient } from '@/lib/api/client';
 import { AppMetadataSchema } from '@insforge/shared-schemas';
 
+// TODO: Replace with schema from shared-schemas
+export interface DatabaseConnectionInfo {
+  connectionURL: string;
+  parameters: {
+    host: string;
+    port: number;
+    database: string;
+    user: string;
+    password: string;
+    sslmode: string;
+  };
+}
+
+export interface DatabasePasswordInfo {
+  databasePassword: string;
+}
+
 export class MetadataService {
   async fetchApiKey() {
     const data = await apiClient.request('/metadata/api-key');
@@ -9,6 +26,18 @@ export class MetadataService {
 
   async getFullMetadata(): Promise<AppMetadataSchema> {
     return apiClient.request('/metadata', {
+      headers: apiClient.withAccessToken(),
+    });
+  }
+
+  async getDatabaseConnectionString(): Promise<DatabaseConnectionInfo> {
+    return apiClient.request('/metadata/database-connection-string', {
+      headers: apiClient.withAccessToken(),
+    });
+  }
+
+  async getDatabasePassword(): Promise<DatabasePasswordInfo> {
+    return apiClient.request('/metadata/database-password', {
       headers: apiClient.withAccessToken(),
     });
   }
