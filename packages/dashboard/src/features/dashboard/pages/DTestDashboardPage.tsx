ca7858765c2861728438b5f85823d2da77ab3592
CarmenDou <15951653662@163.com>
2026-04-26 20:29:07 -0700

    route onboarding redirect to login flow

--- packages/dashboard/src/features/dashboard/pages/DTestDashboardPage.tsx
+++ packages/dashboard/src/features/dashboard/pages/DTestDashboardPage.tsx
@@ -1,31 +1,5 @@
-import { Navigate } from 'react-router-dom';
-import { Skeleton } from '../../../components';
-import { useMcpUsage } from '../../logs/hooks/useMcpUsage';
 import { DTestConnectedDashboard } from '../components/dtest/DTestConnectedDashboard';
 
-function DTestLoadingState() {
-  return (
-    <main className="h-full min-h-0 min-w-0 overflow-y-auto bg-semantic-1">
-      <div className="mx-auto flex w-full max-w-[640px] flex-col gap-6 px-6 pt-16">
-        <Skeleton className="h-10 w-48" />
-        <Skeleton className="h-[140px] w-full rounded" />
-        <Skeleton className="h-[260px] w-full rounded" />
-        <Skeleton className="h-[120px] w-full rounded" />
-      </div>
-    </main>
-  );
-}
-
 export default function DTestDashboardPage() {
-  const { hasCompletedOnboarding, isLoading } = useMcpUsage();
-
-  if (isLoading) {
-    return <DTestLoadingState />;
-  }
-
-  if (!hasCompletedOnboarding) {
-    return <Navigate to="/dashboard/install" replace />;
-  }
-
   return <DTestConnectedDashboard />;
 }
